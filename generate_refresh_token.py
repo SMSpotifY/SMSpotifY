4820970d83c83e1aea91fb28450b2de0b05e7472
Sara Beaudet <sara@saratonin.dev>
2022-01-06 20:23:50 -0500

    hehehe woops

--- generate_refresh_token.py
+++ generate_refresh_token.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client_id = os.environ('SPOTIFY_CLIENT_ID')
-client_secret = os.environ('SPOTIFY_CLIENT_SECRET')
+client_id = os.environ['SPOTIFY_CLIENT_ID']
+client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
 redirect_uri = 'https://example.com/callback'
 
 conf = (client_id, client_secret, redirect_uri)
